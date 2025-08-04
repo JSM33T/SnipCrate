216fa9da9c404cdcdc00eccb17ddd21f9c57c325
jsm33t <jasmeetsinghkainth@gmail.com>
2025-08-04 22:34:34 +0530

    check deployment

---
+++ backend/core-api/SnipCrate.Api/Controllers/Infra/ServerController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace SnipCrate.Api.Controllers.Infra
+{
+    [ApiController]
+    [Route("/api/server")]
+    public class ServerController(ILogger<ServerController> logger) : ControllerBase
+    {
+        private readonly ILogger<ServerController> _logger = logger;
+
+        [HttpGet(Name = "ServerState")]
+        public IActionResult Get()
+        {
+            var systemDetails = new
+            {
+                RuntimeInformation.OSDescription,
+                OSArchitecture = RuntimeInformation.OSArchitecture.ToString(),
+                ProcessArchitecture = RuntimeInformation.ProcessArchitecture.ToString(),
+                Framework = RuntimeInformation.FrameworkDescription,
+                Environment.MachineName,
+                Status = "Up and running"
+            };
+
+            return Ok(systemDetails);
+        }
+    }
+}
