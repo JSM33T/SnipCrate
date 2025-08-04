2d717ad4acf39f07cb45236b34ee662f4b5087a7
jsm33t <jasmeetsinghkainth@gmail.com>
2025-08-04 22:53:54 +0530

    false depoyment trigger

--- backend/core-api/SnipCrate.Api/Controllers/Infra/ServerController.cs
+++ backend/core-api/SnipCrate.Api/Controllers/Infra/ServerController.cs
@@ -19,7 +19,7 @@ namespace SnipCrate.Api.Controllers.Infra
                 ProcessArchitecture = RuntimeInformation.ProcessArchitecture.ToString(),
                 Framework = RuntimeInformation.FrameworkDescription,
                 Environment.MachineName,
-                Status = "Up and running"
+                Status = "Up and running | #2"
             };
 
             return Ok(systemDetails);
