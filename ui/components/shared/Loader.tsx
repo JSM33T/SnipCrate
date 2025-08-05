92ad8c90a39b5c3dc65822a34105d0595a3630f1
jsm33t <jasmeetsinghkainth@gmail.com>
2025-08-05 21:26:50 +0530

    lint:fix

--- ui/components/shared/Loader.tsx
+++ ui/components/shared/Loader.tsx
@@ -101,7 +101,7 @@ export default function Loader() {
                                 className={`w-2 h-2 rounded-full transition-all duration-300 ${index <= currentStep
                                     ? 'bg-primary animate-pulse'
                                     : 'bg-muted'
-                                    }`}
+                                }`}
                             />
                         ))}
                     </div>
